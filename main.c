c8e5b60497000f128cbcbe1025ddabd6cb3638e4
Christian Livbjerg Worsøe <s235111@dtu.dk>
2025-10-20 17:43:27 +0200

    Do a bit more work

--- main.c
+++ main.c
@@ -4,8 +4,9 @@
 
 #define TEST_FILE "./tests/task1/addlarge.bin"
 
-static char MEMORY[0x100000];
-static int32_t REGISTERS[32];
+static char memory[0x100000];
+static int32_t registers[32];
+static uint32_t *program;
 
 void printBinary(unsigned int n, uint8_t len) {
 	for (int i = len - 1; i >= 0; i--) {
@@ -15,13 +16,13 @@ void printBinary(unsigned int n, uint8_t len) {
 }
 
 int main(int argc, char *argv[]) {
-	FILE *fptr;
-	fptr = fopen(TEST_FILE, "rb");
-	fseek(fptr, 0L, SEEK_END);
-	long size = ftell(fptr); // Seek to end and get position
-	fseek(fptr, 0L, SEEK_SET); // Rewind to start
-	uint32_t *program = malloc(size); // we assume file is always 32 bit alligned!
-	fread(program, 1, size, fptr); // For now assume no errors and that everything is read at once
+	FILE *file = fopen(TEST_FILE, "rb");
+	fseek(file, 0L, SEEK_END);
+	long size = ftell(file); // Seek to end and get position
+	fseek(file, 0L, SEEK_SET); // Rewind to start
+	program = malloc(size); // We assume file is always 32 bit aligned!
+	fread(program, 1, size, file); // For now assume no errors and that everything is read at once
+
 	for (int i = 0; i < size / 4; i++) { // DUMP HEX
 		uint32_t instruction = program[i];
 		printf("%08X\t", instruction);
@@ -30,5 +31,11 @@ int main(int argc, char *argv[]) {
 		printBinary(opcode, 8);
 	}
 
+	// Print out all the register values
+	for (int i = 0; i < 32; i++) {
+		printf("x%d = %i (0x%X)", i, registers[i], registers[i]);
+	}
+
+	free(program);
 	return 0;
 }
