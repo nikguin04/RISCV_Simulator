9d5516266bb1d461a917f2ad9230fd25a2eff57e
Christian Livbjerg Worsøe <s235111@dtu.dk>
2025-10-20 16:18:09 +0200

    Fix file reading

--- main.c
+++ main.c
@@ -14,12 +14,12 @@ void printBinary(unsigned int n, uint8_t len) {
 
 int main(int argc, char *argv[]) {
 	FILE *fptr;
-	fptr = fopen(TEST_FILE, "r");
+	fptr = fopen(TEST_FILE, "rb");
 	fseek(fptr, 0L, SEEK_END);
 	long size = ftell(fptr); // Seek to end and get position
 	fseek(fptr, 0L, SEEK_SET); // Rewind to start
 	uint32_t *program = malloc(size); // we assume file is always 32 bit alligned!
-	fgets((char *)program, size, fptr);
+	fread(program, 1, size, fptr); // For now assume no errors and that everything is read at once
 	for (int i = 0; i < size / 4; i++) { // DUMP HEX
 		uint32_t instruction = program[i];
 		printf("%08X\t", instruction);
