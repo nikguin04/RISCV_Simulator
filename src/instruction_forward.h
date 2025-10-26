c0ef4c1db3fc734a236234d47eaeba91d944f082
Niklas Jensen <niklaspj2004@gmail.com>
2025-10-26 19:14:02 +0100

    Added all instruction type declarations

--- src/instruction_forward.h
+++ src/instruction_forward.h
@@ -3,6 +3,11 @@
 
 #include <stdint.h>
 #include "memory.h"
+void handle_r_type(uint8_t rd, uint8_t funct3, uint8_t rs1, uint8_t rs2, uint8_t funct7);
+void handle_i_type(uint8_t rd, uint8_t funct3, uint8_t rs1, uint16_t imm);
+void handle_s_type(uint8_t ls_imm, uint8_t funct3, uint8_t rs1, uint8_t rs2, uint8_t ms_imm); // Least significant and most significant immediates
 void handle_b_type(uint32_t instruction, uint8_t funct3, uint8_t rs1, uint8_t rs2);
+void handle_u_type(uint8_t rd, uint32_t imm, bool opcode_bit_5);
+void handle_j_type(uint8_t rd, uint32_t imm);
 
 #endif
